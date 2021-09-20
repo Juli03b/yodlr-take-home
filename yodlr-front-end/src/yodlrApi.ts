fb831c1d89475ed0e314b4e7f79906d038ec07af
Julio Barrios <julio.junior02@gmail.com>
2021-09-20 15:02:04 -0500

    Add methods for deleting users

--- yodlr-front-end/src/yodlrApi.ts
+++ yodlr-front-end/src/yodlrApi.ts
@@ -1,9 +1,19 @@
-import { User } from "../interfaces";
+import { UserRegister } from "../interfaces";
 import axios, { AxiosResponse } from "axios";
 
 export default class yodlrApi{
     public static async getUsers() {
         const users: AxiosResponse = await axios.get("http://localhost:3000/users");
         return users.data;
+    }
+    public static async register(formData: UserRegister) {
+        const user: AxiosResponse = await axios.post("http://localhost:3000/users", formData);
+        console.log(user)
+        return user.data;
+    }
+    public static async deleteUser(userId: number) {
+        const user: AxiosResponse = await axios.delete(`http://localhost:3000/users/${userId}`);
+        console.log(user)
+        return user;
     }
 }
